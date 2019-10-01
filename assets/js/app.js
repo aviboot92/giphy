d8a0068f0c186d77752ae654e93182d690278771
aviboot92 <avkarra@gmail.com>
2019-10-01 17:08:00 +0530

    Final Product done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,91 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-let animals = ["cat", "dog", "horse", "mouse", "rat", "turtle"];
+    var animals = [
+        "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
+        "bird", "ferret", "turtle", "sugar glider", "chinchilla",
+        "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
+        "capybara", "teacup pig", "serval", "salamander", "frog"
+    ];
 
-// function for getting data
-function getGiphy(animal) {
-    const queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then((response) => {
-        console.log(response);
-        const giphDis = $("#giphs-display");
-        const data = response.data;
-        for(let i=0; i<data.length; i++){
-            const img = $("<img>");
-            img.attr("src", data[i].embed_url);    
-            giphDis.append(img);    
+    // Function to display buttons
+    function displayButtons() {
+        $("#animal-buttons").empty();
+        const buttonsDiv = $("#animal-buttons");
+        for (let i = 0; i < animals.length; i++) {
+            const animalBtn = $("<button>");
+            animalBtn.text(animals[i]);
+            animalBtn.addClass("btn animalButton btn-outline-secondary m-2");
+            buttonsDiv.append(animalBtn);
         }
-    });
-}
-
-// function to generate buttons
-function generateButtons() {
-    $("#buttons-display").empty();
-    const btnDis = $("#buttons-display");
-    for (let index = 0; index < animals.length; index++) {
-        const element = animals[index];
-        const btn = $("<button>");
-        btn.text(element);
-        btn.addClass("giphy btn btn-outline-secondary mr-2");
-        btnDis.append(btn);
     }
-}
 
-generateButtons();
-getGiphy("cat");
+    // Event Listener for Add Animal
+    $("#add-animal").on("click", function (event) {
+        event.preventDefault();
+        const newAnimal = $("#animal-input").val().trim();
+        animals.push(newAnimal);
+        displayButtons();
+        $("#animal-input").val("");
+    });
+
+    // Event Listener to get Animal Giphys
+    $(document).on("click", ".animalButton", function () {
+        $("#animals").empty();
+        // console.log($(this).text());
+        // AJAX call
+        const animalsDiv = $("#animals");
+        var type = $(this).text();
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (res) {
+            const data = res.data;
+            // console.log(data[0]);
+            for (let i = 0; i < res.data.length; i++) {
+                const animalImg = $("<img>");
+                const imgSrcStill = res.data[i].images.fixed_height_still.url;
+                const imgSrcAnim = res.data[i].images.fixed_height_small.url;
+                animalImg.attr("data-animate", imgSrcAnim);
+                animalImg.attr("data-still", imgSrcStill);
+                animalImg.attr("data-state", "still");
+                animalImg.addClass("animal-img");
+                animalImg.attr("src", imgSrcStill);
+                animalImg.addClass("my-2 col-4");
+                animalsDiv.append(animalImg);
+            }
+        })
+    });
+
+    $(document).on("click", ".animal-img", function(){
+        // console.log($(this).attr("data-state"));
+        const steady = $(this).attr("data-still");
+        const animate = $(this).attr("data-animate");
+        const state = $(this).attr("data-state");
+        if(state === "still"){
+            $(this).attr("src", animate);
+            $(this).attr("data-state", "animate");
+        } else{
+            $(this).attr("src", steady);
+            $(this).attr("data-state", "still");
+        }
+    })
+
+    displayButtons();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
